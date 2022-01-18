c5a0595b6e891d4c0fc10d69c2c16f9c608aa7af
Your Name <you@example.com>
2022-01-18 21:38:58 +0800

    Add package using cli

--- hello_world/main.go
+++ hello_world/main.go
@@ -1,10 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"rsc.io/quote"
+)
 
 func main()  {
-	var a int = 10
-	str1 := "go is awesome"
-	fmt.Println("a=",a)
-	fmt.Println(str1)
+	fmt.Println(quote.Hello())
 }
